a06c0c9b605422df919e2f9041708e8a94ac13a0
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-07 17:14:36 +0100

    test dependency (currently failing)

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,6 +1,6 @@
 context("run")
 
-test_that("multiplication works", {
+test_that("can run single script", {
   f <- setup_scenario("simple")
 
   expect_error(source(f("A.R"), local = TRUE), NA)
@@ -10,4 +10,18 @@ test_that("multiplication works", {
   env <- new.env()
   lazyLoad(f("A"), envir = env)
   expect_identical(as.list(env), list(fortytwo = 42))
+})
+
+test_that("can run script with dependency", {
+  f <- setup_scenario("simple")
+
+  expect_error(source(f("B.R"), local = TRUE), NA)
+  expect_true(file.exists(f("A.rdb")))
+  expect_true(file.exists(f("A.rdx")))
+  expect_true(file.exists(f("B.rdb")))
+  expect_true(file.exists(f("B.rdx")))
+
+  env <- new.env()
+  lazyLoad(f("B"), envir = env)
+  expect_identical(as.list(env), list(twentyone = 21))
 })
