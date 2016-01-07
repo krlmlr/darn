e17707aaac0c62d74540c22c98e9caea90f4b7f7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-07 16:05:56 +0100

    basic config

---
+++ R/config.R
@@ -0,0 +1,5 @@
+CONFIG_FILE_NAME <- ".darg"
+
+get_config_file <- function() {
+  find_root_file(CONFIG_FILE_NAME)
+}
--- R/root.R
+++ R/root.R
@@ -1,6 +1,5 @@
-CONFIG_FILE_NAME <- ".darg"
-
 #' @importFrom utils glob2rx
+#' @include config.R
 is_darg_root <- rprojroot::has_file(glob2rx(CONFIG_FILE_NAME))
 
 find_root_file <- rprojroot::make_find_root_file(is_darg_root)
