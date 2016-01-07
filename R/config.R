f34664a0a0163b009a4cb1faa3c867e3ce73c26d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-07 21:31:36 +0100

    PACKAGE_NAME

--- R/config.R
+++ R/config.R
@@ -1,4 +1,6 @@
-CONFIG_FILE_NAME <- ".darg"
+PACKAGE_NAME <- unname(read.dcf("DESCRIPTION")[1L, "Package"])
+
+CONFIG_FILE_NAME <- paste0(".", PACKAGE_NAME)
 
 get_config_file <- function() {
   find_root_file(CONFIG_FILE_NAME)
