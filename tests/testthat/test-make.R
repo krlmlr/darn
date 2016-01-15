6564fc6b2785bf408029ff0f1190a78000478b2f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-15 16:45:54 +0100

    move installation of package outside of test

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -1,17 +1,19 @@
 context("make")
 
-test_that("can make simple project", {
-  f <- setup_scenario("simple")
+withr::with_temp_libpaths({
+  devtools::install(dependencies = FALSE, upgrade_dependencies = FALSE, quiet = TRUE)
+  expect_null(NULL)
 
-  create_makefile(f())
-  withr::with_temp_libpaths({
-    devtools::install(dependencies = FALSE, upgrade_dependencies = FALSE, quiet = TRUE)
+  test_that("can make simple project", {
+    f <- setup_scenario("simple")
+
+    create_makefile(f())
     withr::with_envvar(
       c(R_LIBS=paste(.libPaths(), collapse = ":")),
       {
         expect_equal(run_make("-C", f(), "B.rdx"), 0L)
       }
     )
+    expect_true(file.exists(f("B.rdx")))
   })
-  expect_true(file.exists(f("B.rdx")))
 })
