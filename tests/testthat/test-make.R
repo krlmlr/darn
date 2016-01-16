306be06eea8929c1d336d7520cb2cf69930b0599
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-16 14:57:05 +0100

    indent

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -5,22 +5,22 @@ if (
   && FALSE
   ) {
 
-withr::with_temp_libpaths({
-  devtools::install(dependencies = FALSE, upgrade_dependencies = FALSE, quiet = TRUE)
-  expect_null(NULL)
+  withr::with_temp_libpaths({
+    devtools::install(dependencies = FALSE, upgrade_dependencies = FALSE, quiet = TRUE)
+    expect_null(NULL)
 
-  test_that("can make simple project", {
-    f <- setup_scenario("simple")
+    test_that("can make simple project", {
+      f <- setup_scenario("simple")
 
-    create_makefile(f())
-    withr::with_envvar(
-      c(R_LIBS=paste(.libPaths(), collapse = ":")),
-      {
-        expect_equal(run_make("-C", f(), "B.rdx"), 0L)
-      }
-    )
-    expect_true(file.exists(f("B.rdx")))
+      create_makefile(f())
+      withr::with_envvar(
+        c(R_LIBS=paste(.libPaths(), collapse = ":")),
+        {
+          expect_equal(run_make("-C", f(), "B.rdx"), 0L)
+        }
+      )
+      expect_true(file.exists(f("B.rdx")))
+    })
   })
-})
 
 } # if (TRUE && FALSE) {
