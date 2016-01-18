6c3bda1c7b3ef99d386636b49f0ea2f818ccfc6b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-18 16:19:54 +0100

    test subdir project

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -35,6 +35,20 @@ if (TEST_MAKE) {
       )
       expect_true(file.exists(f("out/B.rdx")))
     })
+
+    test_that("can make subdir project", {
+      f <- setup_scenario("subdir", unlink_darnfile = TRUE)
+
+      create_makefile(f(), src_dir = "dir")
+      withr::with_envvar(
+        c(R_LIBS=paste(.libPaths(), collapse = ":")),
+        {
+          #withr::with_dir(f(), system("xterm"))
+          expect_equal(run_make("-C", f(), "dir/B.rdx"), 0L)
+        }
+      )
+      expect_true(file.exists(f("dir/B.rdx")))
+    })
   })
 
 } # if (TEST_MAKE) {
