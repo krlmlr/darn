6de75d2c92d25678f079022c0f5044fdb8683c61
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-18 16:54:37 +0100

    reorder

--- R/make.R
+++ R/make.R
@@ -8,14 +8,14 @@
 #'   directory), default: \code{"Makefile"}
 #' @param dep_file_name \code{character[1]}\cr File path (relative to the root
 #'   directory), default: \code{"Dependencies"}
-#' @param out_dir,src_dir \code{character[1]}\cr Output and source directories
+#' @param src_dir,out_dir \code{character[1]}\cr Source and output directories
 #'  (relative to the root directory), default: \code{"."}
 #' @param script \code{character[1]}\cr Script that processes the input files,
 #'   default: a call to \code{rmarkdown::\link[rmarkdown]{render}}
 #' @export
 create_makefile <- function(
   root_dir, file_name = "Makefile", dep_file_name = "Dependencies",
-  out_dir = ".", src_dir = ".",
+  src_dir = ".", out_dir = ".",
   script = "Rscript -e \"rmarkdown::render('$<', 'html_document')\"") {
 
   out_dir <- relative_to(file.path(root_dir, out_dir), root_dir)
@@ -26,7 +26,7 @@ create_makefile <- function(
     config_file <-
       MakefileR::makefile() +
       MakefileR::make_comment("This file contains the configuration of the R script network.") +
-      create_config_group(dep_file_name, out_dir, src_dir, script)
+      create_config_group(dep_file_name, src_dir, out_dir, script)
     MakefileR::write_makefile(config_file, config_path)
   }
 
@@ -39,8 +39,8 @@ create_makefile <- function(
       MakefileR::make_comment("Configuration, and default values"),
       MakefileR::make_text(paste0("include ", CONFIG_FILE_NAME)),
       create_config_group(dep_file_name = my_formals$dep_file_name,
-                          out_dir = my_formals$out_dir,
                           src_dir = my_formals$src_dir,
+                          out_dir = my_formals$out_dir,
                           script =  my_formals$script,
                           operator = "?=")
     ) +
@@ -56,12 +56,12 @@ create_makefile <- function(
   MakefileR::write_makefile(make_file, file.path(root_dir, file_name))
 }
 
-create_config_group <- function(dep_file_name, out_dir, src_dir, script,
+create_config_group <- function(dep_file_name, src_dir, out_dir, script,
                                 operator = "=") {
   MakefileR::make_group(
     MakefileR::make_def("dep_file_name", dep_file_name, operator),
-    MakefileR::make_def("out_dir", out_dir, operator),
     MakefileR::make_def("src_dir", src_dir, operator),
+    MakefileR::make_def("out_dir", out_dir, operator),
     MakefileR::make_def("script", script, operator)
   )
 }
