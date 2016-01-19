5adec25edbf0831e9dbd835af853c99322c3d487
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-19 13:27:21 +0100

    expand test
    - Dependencies built without environment variables set, not modified afterwards
    - Two configs

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -69,13 +69,37 @@ if (TEST_MAKE) {
 
       create_makefile(f())
       withr::with_envvar(
-        c(R_LIBS=paste(.libPaths(), collapse = ":"), FORTYTWO="42", TWENTYONE="21"),
+        c(R_LIBS=paste(.libPaths(), collapse = ":")),
         {
+          expect_equal(run_make("-C", f(), "Dependencies"), 0L)
+          expect_true(file.exists(f("Dependencies")))
           #withr::with_dir(f(), system("xterm"))
-          expect_equal(run_make("-C", f()), 0L)
+          dep_file <- readLines(f("Dependencies"))
+
+          expect_equal(run_make("-C", f()), 2L)
+
+          withr::with_envvar(
+            c(FORTYTWO="42", TWENTYONE="21"),
+            {
+              #withr::with_dir(f(), system("xterm"))
+              expect_equal(run_make("-C", f()), 0L)
+            }
+          )
+          expect_identical(dep_file, readLines(f("Dependencies")))
+
+          withr::with_envvar(
+            c(FORTYTWO="43", TWENTYONE="22"),
+            {
+              #withr::with_dir(f(), system("xterm"))
+              expect_equal(run_make("-C", f()), 0L)
+            }
+          )
+          expect_identical(dep_file, readLines(f("Dependencies")))
         }
       )
+
       expect_true(file.exists(f("out/FORTYTWO-42/TWENTYONE-21/src/B.rdx")))
+      expect_true(file.exists(f("out/FORTYTWO-43/TWENTYONE-22/src/B.rdx")))
     })
   })
 
