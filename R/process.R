b002aad130808904b5f106f5351c146850b20c82
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-19 09:54:22 +0100

    support for env_vars in processing code, all tests pass

--- R/process.R
+++ R/process.R
@@ -6,6 +6,7 @@
 #'
 #' @param ... Dependencies
 #' @name init
+#' @inheritParams init_
 NULL
 
 #' @importFrom tibble lst
@@ -21,26 +22,84 @@ get_path_info <- function(path = NULL) {
   config <- read_config(root)
   out_dir <- config[["out_dir"]] %||% "."
 
+  env_vals <- get_env_vals(config)
+  env_dir <- get_env_dir(env_vals)
+
   relative_source_dir <- relative_to(source_dir, root)
 
-  target_dir <- file.path(root, out_dir, relative_source_dir)
+  target_dir <- file.path(root, out_dir, env_dir, relative_source_dir)
 
   target_base <- file.path(target_dir, strip_extension(basename(path)))
 
-  lst(path, source_dir, root, target_dir, target_base)
+  lst(path, source_dir, root, target_dir, target_base, env_vals)
+}
+
+get_env_vals <- function(config) {
+  env_vars <- strsplit(config[["env_vars"]] %||% "", "\\s+")[[1L]]
+  env_vars <- env_vars[env_vars != ""]
+
+  if (length(env_vars) == 0L) {
+    return()
+  }
+
+  env_vals <- Sys.getenv(env_vars, names = TRUE)
+  empty_env_vals <- env_vals[env_vals == ""]
+  if (length(empty_env_vals) > 0L) {
+    stop("Undefined or empty environment variables: ",
+         paste(names(empty_env_vals), collapse = ", "), call. = FALSE)
+  }
+
+  lapply(env_vals, type.convert, as.is = TRUE, numerals = "allow.loss")
+}
+
+get_env_dir <- function(env_vals) {
+  # Special case to work around file.path behavior for zero-length input
+  if (length(env_vals) == 0L) {
+    return("")
+  }
+
+  env_dir_parts <- mapply(function(name, value) paste0(name, "=", "value"),
+                          names(env_vals), env_vals)
+  env_dir <- do.call(file.path, as.list(env_dir_parts))
 }
 
 #' @export
-#' @param .dots \code{list}\cr
+#' @param .dots \code{[list]}\cr
 #'   Additional dependencies as list
+#' @param env_vars \code{[character]}\cr
+#'   Environment variables that define the configuration of the script
 #' @rdname init
-init_ <- function(..., .dots = NULL, envir = parent.frame()) {
+init_ <- function(..., .dots = NULL, env_vars = NULL,
+                  envir = parent.frame()) {
   path_info <- get_path_info()
+
+  check_env_vals(env_vars, path_info)
 
   deps_list <- get_init_deps_list(.dots, ...)
 
   mapply(init_one, names(deps_list), deps_list, MoreArgs = list(
     path_info = path_info, envir = envir))
+
+  assign_env_vals(path_info$env_vals, envir)
+}
+
+check_env_vals <- function(env_vars, path_info) {
+  extra_env_vars <- setdiff(env_vars, names(path_info$env_vals))
+  if (length(extra_env_vars) > 0L) {
+    stop("Define all environment variables in the ", CONFIG_FILE_NAME,
+         ", missing: ", paste(extra_env_vars, collapse =", "), ".",
+         call. = FALSE)
+  }
+}
+
+assign_env_vals <- function(env_vals, envir) {
+  defined_env_vars <- intersect(names(env_vals), ls(envir))
+  if (length(defined_env_vars) > 0L) {
+    stop("Name clash for environment variables: ",
+         paste0(defined_env_vars, collapse = ", "), call. = FALSE)
+  }
+
+  mapply(assign, names(env_vals), env_vals, MoreArgs = list(envir = envir))
 }
 
 get_init_deps_list <- function(.dots, ...) {
