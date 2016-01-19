07998c59a2a9b0b36d51e0aed6922541affa5379
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-19 10:01:52 +0100

    support env_vars arg to create_makefile()

--- R/make.R
+++ R/make.R
@@ -3,19 +3,24 @@
 #' This function creates a Makefile that builds all scripts below the root
 #' directory.
 #'
-#' @param root_dir \code{character[1]}\cr The root directory
-#' @param file_name \code{character[1]}\cr File path (relative to the root
-#'   directory), default: \code{"Makefile"}
-#' @param dep_file_name \code{character[1]}\cr File path (relative to the root
-#'   directory), default: \code{"Dependencies"}
-#' @param src_dir,out_dir \code{character[1]}\cr Source and output directories
-#'  (relative to the root directory), default: \code{"."}
-#' @param script \code{character[1]}\cr Script that processes the input files,
+#' @param root_dir \code{[character(1)]}\cr
+#'   The root directory
+#' @param file_name \code{[character(1)]}\cr
+#'   File path (relative to the root directory), default: \code{"Makefile"}
+#' @param dep_file_name \code{[character(1)]}\cr
+#'   File path (relative to the root directory), default: \code{"Dependencies"}
+#' @param src_dir,out_dir \code{[character(1)]}\cr
+#'   Source and output directories (relative to the root directory), default:
+#'   \code{"."}
+#' @param env_vars \code{[character]}\cr
+#'   Names of environment variables that define configurations of the script,
+#'   default: none
+#' @param script \code{[character]}\cr Script that processes the input files,
 #'   default: a call to \code{rmarkdown::\link[rmarkdown]{render}}
 #' @export
 create_makefile <- function(
   root_dir, file_name = "Makefile", dep_file_name = "Dependencies",
-  src_dir = ".", out_dir = ".",
+  src_dir = ".", out_dir = ".", env_vars = NULL,
   script = "Rscript -e \"rmarkdown::render('$<', 'html_document')\"") {
 
   out_dir <- relative_to(file.path(root_dir, out_dir), root_dir)
@@ -26,7 +31,7 @@ create_makefile <- function(
     config_file <-
       MakefileR::makefile() +
       MakefileR::make_comment("This file contains the configuration of the R script network.") +
-      create_config_group(dep_file_name, src_dir, out_dir, script)
+      create_config_group(dep_file_name, src_dir, out_dir, env_vars, script)
     MakefileR::write_makefile(config_file, config_path)
   }
 
@@ -41,6 +46,7 @@ create_makefile <- function(
       create_config_group(dep_file_name = my_formals$dep_file_name,
                           src_dir = my_formals$src_dir,
                           out_dir = my_formals$out_dir,
+                          env_vars = my_formals$env_vars,
                           script =  my_formals$script,
                           operator = "?=")
     ) +
@@ -56,12 +62,13 @@ create_makefile <- function(
   MakefileR::write_makefile(make_file, file.path(root_dir, file_name))
 }
 
-create_config_group <- function(dep_file_name, src_dir, out_dir, script,
-                                operator = "=") {
+create_config_group <- function(dep_file_name, src_dir, out_dir, env_vars,
+                                script, operator = "=") {
   MakefileR::make_group(
     MakefileR::make_def("dep_file_name", dep_file_name, operator),
     MakefileR::make_def("src_dir", src_dir, operator),
     MakefileR::make_def("out_dir", out_dir, operator),
+    MakefileR::make_def("env_vars", env_vars %||% "", operator),
     MakefileR::make_def("script", script, operator)
   )
 }
