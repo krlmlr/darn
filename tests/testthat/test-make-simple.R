02dd42d2a3c2bc96b805d1dfa90484c6650ff351
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-07-07 21:01:48 +0200

    unify simple and src-dir tests

--- tests/testthat/test-make-simple.R
+++ tests/testthat/test-make-simple.R
@@ -8,10 +8,13 @@ withr::with_temp_libpaths({
 
   test_that("can make simple project", {
     scenario_name <- "simple"
+    src_dir <- "."
     out_dir <- "."
-    f <- setup_scenario(scenario_name)
+    unlink_darnfile <- FALSE
+    makefile_warning <- "Not overwriting"
+    f <- setup_scenario(scenario_name, unlink_darnfile)
 
-    expect_warning(create_makefile(f()), "Not overwriting")
+    expect_warning(create_makefile(f(), src_dir = src_dir), makefile_warning)
 
     expect_false(file.exists(f("Dependencies")))
 
@@ -25,7 +28,7 @@ withr::with_temp_libpaths({
     expect_true(file.exists(f("Dependencies")))
     expect_true(file.exists(f(out_dir, "B.rdx")))
 
-    writeLines("darn::done()", f("C.R"))
+    writeLines("darn::done()", f(src_dir, "C.R"))
     withr::with_envvar(
       c(R_LIBS=paste(.libPaths(), collapse = ":")),
       {
@@ -46,9 +49,9 @@ withr::with_temp_libpaths({
     )
     expect_false(all(grepl("B[.]R", readLines(f("Dependencies")))))
 
-    expect_lt_time(file.info(f("A.R"))$mtime, file.info(f("Dependencies"))$mtime)
-    writeLines(readLines(f("A.R")), f("A.R"))
-    expect_gt_time(file.info(f("A.R"))$mtime, file.info(f("Dependencies"))$mtime)
+    expect_lt_time(file.info(f(src_dir, "A.R"))$mtime, file.info(f("Dependencies"))$mtime)
+    writeLines(readLines(f(src_dir, "A.R")), f(src_dir, "A.R"))
+    expect_gt_time(file.info(f(src_dir, "A.R"))$mtime, file.info(f("Dependencies"))$mtime)
     withr::with_envvar(
       c(R_LIBS=paste(.libPaths(), collapse = ":")),
       {
@@ -56,6 +59,6 @@ withr::with_temp_libpaths({
         expect_equal(run_make("-C", f(), "--dry-run"), 0L)
       }
     )
-    expect_lt_time(file.info(f("A.R"))$mtime, file.info(f("Dependencies"))$mtime)
+    expect_lt_time(file.info(f(src_dir, "A.R"))$mtime, file.info(f("Dependencies"))$mtime)
   })
 })
